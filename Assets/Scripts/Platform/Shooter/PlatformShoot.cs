083b43c4cc2768eb92c49891ac3c9980e847b6b2
Adi Zhavo <adi@gram.gs>
2016-04-16 19:10:00 +0300

    Created Platform shooter and Concrete States

---
+++ Assets/Scripts/Platform/Shooter/PlatformShoot.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformShoot : MonoBehaviour
+{
+    #region Public Fields
+    public Vector3 Position { get { return transform.position; } }
+
+    public Vector2 MinMaxAngles { get { return minMaxAngles; } }
+
+    public float MinDistanceOfTouch { get { return minDistanceOfTouch; } }
+
+    public float MaxDragDistance { get { return maxDragDistance; } }
+
+    public float ShootForceMultiplier { get { return shootForceMultiplier; } }
+
+    public string FactoryCallCode { get { return factoryCallCode; } }
+    #endregion
+
+    #region Private Fields
+    [SerializeField] protected Vector2 minMaxAngles;
+    [SerializeField] protected float minDistanceOfTouch;
+    [SerializeField] protected float maxDragDistance;
+    [SerializeField] protected float shootForceMultiplier;
+    [SerializeField] protected string factoryCallCode = "ShootParticle";
+    #endregion
+
+    protected IShootStates currentState;
+
+    private void Awake()
+    {
+        InitializeStates();
+    }
+
+    private void Update()
+    {
+        currentState.ShootFrameCheck();
+    }
+
+    protected virtual void InitializeStates()
+    {
+        currentState = new DetectTouchPos(this);
+    }
+
+    public void ChangeShooterState(IShootStates newState)
+    {
+        currentState = newState;
+    }
+
+    public void Shoot(DirectionVector dirVect)
+    {
+        GameObject particle = ObjectFactory.Instance.CreateObjectCode(FactoryCallCode) as GameObject;
+
+        if (particle != null)
+        {
+            ShootBullet(particle.GetComponent<BulletMovement>(), dirVect);
+        }
+    }
+
+    protected virtual void ShootBullet(BulletMovement bullet, DirectionVector dirVect)
+    {
+        if (bullet != null)
+        {
+            DirectionVector multipliedDirVect = new DirectionVector(dirVect.direction * shootForceMultiplier);
+            bullet.SetDirection(multipliedDirVect);
+        }
+        else
+        {
+            Debug.LogWarning("You tried to shoot not a bullet, check you call to the factory!");
+        }
+    }
+}
---
+++ Assets/Scripts/Platform/Shooter/ShooterStates.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IShootStates
+{
+    void ShootFrameCheck();
+}
+
+public class DetectTouchPos : IShootStates
+{
+    private PlatformShoot platform;
+
+    public DetectTouchPos(PlatformShoot platformTr)
+    {
+        this.platform = platformTr;
+    }
+
+    public void ShootFrameCheck()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            float touchDistance = MathCalc.GetTouchDistance(platform.Position).magnitudeOfDir;
+
+            if (touchDistance < platform.MinDistanceOfTouch)
+            {
+                platform.ChangeShooterState(new AimDragger(platform));
+            }
+        }
+    }
+}
+
+public class AimDragger : IShootStates
+{
+    private PlatformShoot platform;
+    private DirectionVector shootDirValues;
+    private Vector3 DeltaMove;
+
+    public AimDragger(PlatformShoot platform)
+    {
+        this.platform = platform;
+    }
+
+    public void ShootFrameCheck()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            SetNewShootDirection();
+            DebugDragVector();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            DirectionVector shVal = new DirectionVector(-1 * shootDirValues.direction, shootDirValues.magnitudeOfDir);
+            platform.ChangeShooterState(new ObjectShooter(platform, ref shVal));
+        }
+    }
+
+    private void SetNewShootDirection()
+    {
+        DirectionVector currentCalcDir = MathCalc.GetTouchDistance(platform.Position);
+        MathCalc.ClampVectMagnitude(ref currentCalcDir, platform.MaxDragDistance);
+        if (!IsAngleOutRange(currentCalcDir))
+        {
+            shootDirValues = currentCalcDir;
+        }
+    }
+
+    private bool IsAngleOutRange(DirectionVector currentCalcDir)
+    {
+        float currentAngle = Vector2.Angle(Vector2.right, (Vector2)platform.Position + currentCalcDir.direction);
+
+        bool isOut = (platform.Position.y < currentCalcDir.direction.y
+            || currentAngle > platform.MinMaxAngles.y
+            || currentAngle < platform.MinMaxAngles.x);
+
+        return isOut;
+    }
+
+    private void DebugDragVector()
+    {
+        #if UNITY_EDITOR
+
+        Debug.DrawLine(platform.Position, platform.Position + (Vector3)shootDirValues.direction, Color.yellow);
+
+        #endif
+    }
+}
+
+public class ObjectShooter : IShootStates
+{
+    private PlatformShoot platform;
+    private DirectionVector shootDirValues;
+
+    public ObjectShooter(PlatformShoot platform, ref DirectionVector shootDirValues)
+    {
+        this.platform = platform;
+        this.shootDirValues = shootDirValues;
+    }
+
+    public void ShootFrameCheck()
+    {
+        platform.Shoot(shootDirValues);
+        platform.ChangeShooterState(new DetectTouchPos(platform));
+    }
+}
