6d94eb2c94484a2dec60cc7f93a159c1a1aca6e1
Adi Zhavo <adi@gram.gs>
2016-04-16 19:08:03 +0300

    Crated pool & factory

---
+++ Assets/Scripts/Utils/ObjectCreation/ObjectFactory.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectFactory : MonoBehaviour
+{
+    public static ObjectFactory Instance;
+
+    [SerializeField] private PoolType[] Pools;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public GameObject CreateObjectCode(string PoolCode)
+    {
+        for (int pool = 0; pool < Pools.Length; pool++)
+        {
+            if (PoolCode.Equals(Pools[pool].CallCode))
+            {
+                return Pools[pool].Pool.GetObjectInPool();
+            }
+        }
+
+        return null;
+    }
+}
+
+[System.Serializable]
+public struct PoolType
+{
+    public string CallCode;
+    public ObjectPool Pool;
+}
