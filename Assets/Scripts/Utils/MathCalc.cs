d7ad86f8078df5004b701b1867ee06927aa3327e
Adi Zhavo <adi@gram.gs>
2016-04-16 19:09:16 +0300

    Created handy math calculations

---
+++ Assets/Scripts/Utils/MathCalc.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class MathCalc
+{
+    public static DirectionVector GetTouchDistance(Transform fromPos)
+    {
+        Vector2 wordTouchPos = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float touchDistance = Vector2.Distance(fromPos.position, wordTouchPos);
+        return new DirectionVector(wordTouchPos, touchDistance);
+    }
+
+    public static DirectionVector GetTouchDistance(Vector3 fromPos)
+    {
+        Vector2 wordTouchPos = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float touchDistance = Vector2.Distance(fromPos, wordTouchPos);
+        return new DirectionVector(wordTouchPos, touchDistance);
+    }
+
+    public static void ClampVectMagnitude(ref DirectionVector dirVect, float maxMagnitude)
+    {
+        if (dirVect.magnitudeOfDir > maxMagnitude)
+        {
+            dirVect.direction = Vector3.ClampMagnitude(dirVect.direction, maxMagnitude);
+        }
+    }
+
+    public static Vector2 GetDeltaBetweenDirVector(DirectionVector fromDir, DirectionVector toDir)
+    {
+        return toDir.direction - fromDir.direction;
+    }
+}
+
+public struct DirectionVector
+{
+    public Vector2 direction;
+    public float magnitudeOfDir;
+
+    public DirectionVector(Vector2 worldTouchPos, float touchDistance)
+    {
+        this.direction = worldTouchPos;
+        this.magnitudeOfDir = touchDistance;
+    }
+
+    public DirectionVector(Vector2 worldTouchPos)
+    {
+        this.direction = worldTouchPos;
+        this.magnitudeOfDir = direction.magnitude;
+    }
+}
