db7ad789b2141f6dd1a46a1ba7195fa9de2f6432
Adi Zhavo <adi@gram.gs>
2016-04-16 19:08:46 +0300

    Created bullet Movement logic

---
+++ Assets/Scripts/Bullet/BulletMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMovement : MonoBehaviour {
+
+    protected DirectionVector dirShoot;
+
+    public void SetDirection(DirectionVector dirShoot)
+    {
+        this.dirShoot = dirShoot;
+    }
+
+    protected void Update()
+    {
+        CalculateCurrentPos();
+    }
+
+    protected virtual void CalculateCurrentPos()
+    {
+        transform.position += (Vector3)(dirShoot.direction * Time.deltaTime);
+        dirShoot.direction += Physics2D.gravity * Time.deltaTime;
+    }
+}
