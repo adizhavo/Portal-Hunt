a281585032d91a313f2a0d998d9909745faef37a
Adi Zhavo <adi@gram.gs>
2016-06-01 17:42:17 +0300

    Added map frame

---
+++ Assets/AnchorToUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnchorToUI : MonoBehaviour {
+
+    [SerializeField] private Transform AnchoredTransform;
+
+    private void Awake()
+    {
+        transform.position = AnchoredTransform.position;
+    }
+
+    #if UNITY_EDITOR
+
+    private void LateUpdate()
+    {
+        Awake();
+    }
+
+    #endif
+}
