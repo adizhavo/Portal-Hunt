7ce1fc02f22feea1689dd0209608f1188203fa99
Adi Zhavo <adi@gram.gs>
2016-06-02 18:00:11 +0300

    Added Touch input for multitouch support

---
+++ Assets/Scripts/Utils/TouchInput.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public static class TouchInput 
+{
+    private static bool Multitouch = false;
+
+    public static bool TouchDown()
+    {
+        if (!Multitouch)
+            return Input.GetMouseButtonDown(0);
+        else
+        {
+            // TODO : Add multitouch support
+            return false;
+        }
+
+        return false;
+    }
+
+    public static bool Touch()
+    {
+        if (!Multitouch)
+            return Input.GetMouseButton(0);
+        else
+        {
+            // TODO : Add multitouch support
+            return false;
+        }
+
+        return false;
+    }
+
+    public static bool TouchUp()
+    {
+        if (!Multitouch)
+            return Input.GetMouseButtonUp(0);
+        else
+        {
+            // TODO : Add multitouch support
+            return false;
+        }
+
+        return false;
+    }
+}
