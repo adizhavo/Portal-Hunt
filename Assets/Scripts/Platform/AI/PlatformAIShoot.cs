004556fadadb2cecfa96a1c69357247c905bc403
Adi Zhavo <adi@gram.gs>
2016-06-11 19:02:01 +0300

    AI learns while you play, and plays with you

---
+++ Assets/Scripts/Platform/AI/PlatformAIShoot.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformAIShoot : PlatformShoot {
+
+    [SerializeField] private Vector2 AimTime;
+    [SerializeField] protected PlatformMovement pltMover;
+    private PlatformPositionReader posReader;
+
+    private float currentAimTime;
+    private float timeCounter;
+
+    public override void Init()
+    {
+        posReader = new PlatformPositionReader(pltMover.HorizontalBoundaries, pltMover.VerticalBoundaries);
+        ChooseAimTime();
+    }
+
+    private void ChooseAimTime()
+    {
+        currentAimTime = Random.Range(AimTime.x, AimTime.y);
+        timeCounter = 0f;
+    }
+
+    private void Shoot()
+    {
+        PlatformPos currentPos = posReader.GetPosition(Position2D);
+        int currentMapState = MapStateAccessor.Instance.GetActiveState();
+        DirectionVector shootVector = AIDatabase.Instance.GetShot(currentPos, currentMapState);
+
+        if (shootVector.magnitudeOfDir > Mathf.Epsilon) base.Shoot(shootVector);
+    }
+
+    private void Update()
+    {
+        if (timeCounter < currentAimTime)
+            timeCounter += Time.deltaTime;
+        else
+        {
+            Shoot();
+            ChooseAimTime();
+        }
+    }
+}
